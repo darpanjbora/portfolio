24564c14bb75a24f5cf8362be01372f45917121e
Darpan Jyoti Bora <darpanjyoti.bora@gmail.com>
2019-06-07 23:08:21 +0530

    Updates

---
+++ precache-manifest.d5b82b6d2b23322d8f2a54b4e2ba49c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e2b16b75af33bb23234318bc8c1bc9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "68e4ee95a652788dd4ab",
+    "url": "/portfolio/static/css/2.400ebc83.chunk.css"
+  },
+  {
+    "revision": "f39749d4ef4b14b6179e",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "68e4ee95a652788dd4ab",
+    "url": "/portfolio/static/js/2.25142656.chunk.js"
+  },
+  {
+    "revision": "f39749d4ef4b14b6179e",
+    "url": "/portfolio/static/js/main.72c8e975.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
